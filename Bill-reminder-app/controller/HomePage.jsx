305634d50bc313e991588158a4a4f1767140f147
MukeshChoudhury-coder <mukeshchoudhury61@gmail.com>
2026-02-22 18:18:28 +0530

    Update HomePage.jsx

--- Bill-reminder-app/controller/HomePage.jsx
+++ Bill-reminder-app/controller/HomePage.jsx
@@ -1,17 +1,42 @@
+import { useCallback, useContext } from "react";
+import { proContext } from "../Provider/Provider";
+import { useState } from "react";
+import { Link } from "react-router-dom"; 
+import Mainbody from "./MainBody";
 
 function HomePage(){
+    const [on ,setOn]=useState("none")
+
+    const {loginData}=useContext(proContext)
+
+    const  onOff=useCallback(()=>{
+       if(on==="none"){
+         setOn("block")
+       }else{
+        setOn("none")
+       }
+    }, [on])
+
     return(
         <>
     <nav>
         <div id="nav-child1">
-            <i class="fa-solid fa-money-bill" ></i>
+            <i className="fa-solid fa-money-bill" ></i>
             <p>Bill Reminder</p>
         </div>
 
         <div id="nav-child2"> 
-            <p></p>
+            <button id="acc-btn" onClick={onOff} >{loginData}</button>
+
+            <div id="edit" style={{display: on}}>
+                <Link to={"/login"}><button className="edit-btn">Create Account</button></Link>
+
+                <button className="edit-btn">Archived</button>
+            </div>
         </div>
     </nav>
+
+     <Mainbody/>
         </>
     )
 }
