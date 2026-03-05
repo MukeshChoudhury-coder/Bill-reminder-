1c78bbe800c527e97c47ab5511a97f57742f861e
MukeshChoudhury-coder <mukeshchoudhury61@gmail.com>
2026-03-05 06:53:50 +0530

    Update Provider.jsx

--- Bill-reminder-app/Provider/Provider.jsx
+++ Bill-reminder-app/Provider/Provider.jsx
@@ -3,7 +3,6 @@ import { createContext, useEffect, useMemo, useState, useCallback } from "react"
 export const proContext=createContext()
 
 function Provider({children}){
-
     //--- display ; hidden bill form input--/
     const [display, setDisplay]=useState("none")
     const ShowInptField=useCallback(()=>{
@@ -38,7 +37,7 @@ useEffect(()=>{
    })
    .catch(err=> console.log(err))
 
- },[])
+ },[billData])
  
  //------------------------------------//
 
@@ -51,7 +50,7 @@ useEffect(()=>{
 
     setBillAmount(total)
    
- },[])
+ },[billData])
 
 //----------------------------------------//
 
@@ -69,13 +68,92 @@ setDisplay("block")
 
 
 //----DELETE DATA FOR BILL.TXT------//
+const [idd, setId]=useState()
+ function getDeleteId(value){
+    setId(value)
+}
+
+useEffect(()=>{
+   if(!idd){
+    return
+   }
+   fetch(`http://localhost:3001/deleteapi?id=${idd}`,{
+    method:"DELETE"
+   })
+   .then(res=>res.json())
+   .then(data=>{
+    console.log(data.dltmsg)
+     setBillData(prev=>prev.filter(item=> item.id!==idd))
+   })
+   .catch(err=>console.log(err))
+},[idd])
+//--------------------------------------------------//
+   //--------OVER DUE CALCULATION---------------//
+const [overDue, setOverDue]=useState(0)
+
+useMemo(()=>{
+    let checkOverDue=billData.filter((d)=>{
+        return d.status==="Over Due"
+    })
 
 
+    let overDues=checkOverDue.reduce((acc, sum)=>{
+             return acc+=Number(sum.userAmount)
+    }, 0)
+    setOverDue(overDues)
 
+},[billData])
+//---------------------------------------------------------//
+  //--ADDING PAID FEATURE--//
+  const [paidId, getPaidIds]=useState()
+  function  getPiadId(paidVal){
+ getPaidIds(paidVal)
+  }
+  useEffect(()=>{
+  if(!paidId){
+    return 
+  }
+    fetch(`http://localhost:3001/paidapi?id=${paidId}`,{method:"PUT"})
+  .then(res=>res.json())
+  .then(data=>console.log(data.paidStatus))
+  .catch(err=>console.log(err))
+ 
+  },[paidId])
+  //-------------------------------------------------------------------//
+    //--- FILTERING PAID DATA FROM billData-----//
+    const [paidData, setPaidData]=useState([])
+ let getPaidBills=useEffect(()=>{
+    let getPaid
+      if(billData){
+           getPaid=billData.filter((p)=>{
+            return p.status==="PAID"
+        })
 
-    return(
+      }
+
+        if(!getPaid){
+       return
+       }else{
+         setPaidData(getPaid)
+       }
+    }, [billData])
+  //---------------------------------------------------------//
+    //-------FILTERING OVERDUE BILLS FROM billData---------//
+    const [overDueBill, setOverDueBill]=useState([])
+    useEffect(()=>{
+        let getDueBills=billData.filter((b)=>{
+          return b.status==="Over Due"
+        })
+
+        if(!getDueBills){
+          return;
+        }
+        setOverDueBill(getDueBills)
+    },[billData])
+   
+  return(
         <>
-        <proContext.Provider value={{loginData,getData,billData, totalBillAmount,geteditInpt, ShowInptField,display, editedData, setBillData}}>
+        <proContext.Provider value={{loginData,getData,billData, totalBillAmount,geteditInpt, ShowInptField,display, editedData, setBillData,getDeleteId,overDue,getPiadId,paidData,getPaidBills,overDueBill}}>
             {children}
         </proContext.Provider>
         </>
