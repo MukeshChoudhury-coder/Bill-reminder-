69d58a6289cc6ac947b68fc13dd63fbc32a83f27
MukeshChoudhury-coder <mukeshchoudhury61@gmail.com>
2026-03-05 07:01:04 +0530

    Update App.jsx

--- Bill-reminder-app/src/App.jsx
+++ Bill-reminder-app/src/App.jsx
@@ -2,17 +2,21 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom"
 import HomePage from "../controller/HomePage";
 import CreateAccount from "../controller/CreateAccount"
+import PaidSec from "../filtersection/PaidSec";
+import OverDueSec from "../filtersection/OverDueSec";
+
 function App() {
 return(
   <>
    <Routes>
     <Route path="/"  element={<HomePage></HomePage>}></Route>
     <Route path="/login"  element={<CreateAccount></CreateAccount>}></Route>
-   </Routes>
+    <Route path="/paid" element={<PaidSec></PaidSec>}></Route>
+    <Route path="/due" element={<OverDueSec></OverDueSec>}></Route>
+   </Routes> 
  
   </>
 )
 }
 
 export default App;
- 
