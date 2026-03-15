2ea49d7f99414bc66e5e237f465aee1c51d125db
MukeshChoudhury-coder <mukeshchoudhury61@gmail.com>
2026-03-15 23:03:38 +0530

    Update HomePage.jsx

--- Bill-reminder-app/controller/HomePage.jsx
+++ Bill-reminder-app/controller/HomePage.jsx
@@ -7,7 +7,7 @@ import Mainbody from "./MainBody";
 function HomePage(){
     const [on ,setOn]=useState("none")
 
-    const {loginData, getPaidBills}=useContext(proContext)
+    const {loginData, getPaidBills, getSearch,searchVal,setSearchText,searchText,getSearchId}=useContext(proContext)
 
     const  onOff=useCallback(()=>{
        if(on==="none"){
@@ -49,6 +49,33 @@ function HomePage(){
             </div>
         </div>
     </nav>
+  <div>
+      <form onSubmit={(e)=>e.preventDefault()}>
+        <input type="text" placeholder="Search by bill..." 
+        value={searchText}
+       onChange={(e)=>{
+ setSearchText(e.target.value)
+ getSearch(e.target.value)
+}}
+        ></input>
+        <button >Search</button>
+    </form>
+
+    <div id="Search-result">
+      
+       
+        {Array.isArray(searchVal)&& searchVal.map((s)=>{
+         return <Link to={"/search-section"}>
+         <div key={s.id} id="search-data" onClick={()=>getSearchId(s.id)}>
+                <p>{s.userBill}</p>
+                <p>{s.userAmount}</p>
+                <p>{s.userDate}</p>
+                <p>{s.status}</p>
+            </div>
+         </Link>
+        })}
+    </div>
+  </div>
 
    {loginData==="Please Create an Account" ? <div className="create-profile-msg-btn">
     <h1 id="create-profile-text">Create your profile first</h1> 
@@ -59,5 +86,3 @@ function HomePage(){
     )
 }
 export default HomePage;
-
-
